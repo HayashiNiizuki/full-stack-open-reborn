4923e5b346b9941949f79df2caa9f4415bec66e0
Hayashi Niizuki on Ubuntu <hayashi_niizuki@outlook.com>
2024-09-07 21:59:36 +0800

    Part-6.b class

--- Part-6/redux-notes/src/App.js
+++ Part-6/redux-notes/src/App.js
@@ -1,11 +1,13 @@
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
-
+import VisibilityFilter from './components/VisibilityFilter'
+import { filterChange } from './reducers/filterReducer'
 const App = () => {
   return (
     <div>
       <NewNote />
-      <Notes  />
+      <VisibilityFilter/>
+      <Notes />
     </div>
   )
 }
